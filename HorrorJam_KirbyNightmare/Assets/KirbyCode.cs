a0ea065a4d3e5f1d0e81cd3f9cb646d8cab6de20
Gerard Vega <vega.gerard@yahoo.com>
2015-10-24 05:24:19 -0400

    All of Kirby functionality is skeleton'd or staged just need enemies to test.
    Still need to have abilities and stuff. Went to take a nap for a bit.

--- HorrorJam_KirbyNightmare/Assets/KirbyCode.cs
+++ HorrorJam_KirbyNightmare/Assets/KirbyCode.cs
@@ -5,12 +5,13 @@ public class KirbyCode : MonoBehaviour {
 
 	// Use this for initialization
 
+    public int healthcount = 6;
+    public int lives = 4;
     private bool reversed;
     private Vector2 localVel;
     private int jumpcount;
     private Sprite Avatar;
     private bool IsCrouched = false;
-<<<<<<< HEAD
     public LayerMask layer;
     private bool iSGrounded;
     private bool isDashing;
@@ -18,23 +19,17 @@ public class KirbyCode : MonoBehaviour {
     public float dashed = 2.0f;
     private float _lastbuttonpress;
     private float _lastdash;
+    Animator anim;
 	void Start () 
     {
         localVel = GetComponent<Rigidbody2D>().velocity;
         Avatar = GetComponent<SpriteRenderer>().sprite;
         reversed = false;
         isDashing = false;
-	}
-=======
-    Animator anim;
-
-    void Start () 
-    {
-        localVel = GetComponent<Rigidbody2D>().velocity;
-        Avatar = GetComponent<SpriteRenderer>().sprite;
         anim = GetComponent<Animator>();
-    }
->>>>>>> refs/remotes/origin/Jose
+
+	}
+
 	
 	// Update is called once per frame
 	void Update () 
@@ -46,11 +41,32 @@ public class KirbyCode : MonoBehaviour {
             jumpcount = 0;
         }
 
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            healthcount--;
+            //if(something is in mouth or has ability)
+            //{ }
+        }
+    }
+    
+
+    void FixedUpdate()
+    {
+
+        //float move = Input.GetAxis("Horizontal");
+
+        //anim.SetFloat("move", Mathf.Abs(move));
+
         //WASD controls
-	    if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             //Jump
-            if(jumpcount == 0 && iSGrounded)
+            if (jumpcount == 0 && iSGrounded)
             {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 225.0f));
                 //transform.Translate(new Vector3(0.0f, 0.5f, 0.0f));
@@ -62,18 +78,18 @@ public class KirbyCode : MonoBehaviour {
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 125.0f));
                 //transform.Translate(new Vector3(0.0f, 0.4f, 0.0f));
                 jumpcount++;
-                if(jumpcount == 5)
+                if (jumpcount == 5)
                 {
                     //Avatar = sprite that puffs air;
                 }
             }
-            
+
 
             //EnterDoorWays
             //Application.LoadLevel("EnterLevelOrIDnum");
         }
 
-	    if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
 
             transform.Translate(new Vector3(-1.0f * Time.deltaTime, 0.0f, 0.0f));
@@ -82,46 +98,62 @@ public class KirbyCode : MonoBehaviour {
                 transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
                 reversed = true;
             }
+            GetComponent<Animator>().SetBool("move", true);
         }
-	    else if(Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             //GetComponent<SpriteRenderer>().sprite = crouching;
-            
+
         }
-	    else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             
-            if(isDashPossible && Input.GetKeyUp(KeyCode.D))
-            {
-
-            }
-            else
             {
                 //Avatar = walking
                 transform.Translate(new Vector3(1.0f * Time.deltaTime, 0.0f, 0.0f));
                 if (reversed)
                 {
-                    transform.localScale = new Vector3(transform.localScale.x * -1.0f, 0.0f, transform.localScale.z);
+                    transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
                     reversed = false;
                 }
             }
+            GetComponent<Animator>().SetBool("move", true);
 
 
         }
-        
+        else
+        {
+            GetComponent<Animator>().SetBool("move", false);
+        }
 
         //Action buttons
-	    if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            
+            if(IsCrouched && Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(4.0f, 0.0f, 0.0f));
+                //GetComponent<Animator>().SetBool("StringSlide", true);
+            }
+            else if (IsCrouched && Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-4.0f, 0.0f, 0.0f));
+                //GetComponent<Animator>().SetBool("StringSlide", true);
+            }
+
+            //Sucking Code
+
+            //If something in mouth
+
+            //If player has an ability
         }
 
         //Extra Button
         //if(Input.GetKey(KeyCode.RightShift))
         //{
-
+              //Release Ability
         //}
-	}
+    }
+
 
     float VelLimit(float velocity)
     {
@@ -129,7 +161,6 @@ public class KirbyCode : MonoBehaviour {
         return velocity;
     }
 
-<<<<<<< HEAD
     bool isDashPossible
     {
         get
@@ -137,16 +168,12 @@ public class KirbyCode : MonoBehaviour {
             return Time.time - _lastdash > dashed;
         }
     }
-=======
-    void FixedUpdate()
+
+     void DoDash()
     {
-
-        float move = Input.GetAxis("Horizontal");
-
-        anim.SetFloat("move", Mathf.Abs(move));
+        Debug.Log("In DoDash()");
+        _lastdash = Time.time;
+        transform.Translate(4.0f * Time.deltaTime, 0.0f, 0.0f);
     }
 
-
-
->>>>>>> refs/remotes/origin/Jose
 }
