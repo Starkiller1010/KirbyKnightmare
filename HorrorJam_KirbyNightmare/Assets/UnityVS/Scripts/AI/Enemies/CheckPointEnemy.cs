90bb45b28bb70de9af43de04c4c68fc5874fa836
HackerEva <565430601@qq.com>
2015-10-24 19:03:59 -0400

    Yangjie Yao
    Finished Suck

---
+++ HorrorJam_KirbyNightmare/Assets/UnityVS/Scripts/AI/Enemies/CheckPointEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointEnemy : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.tag == "Underground")
+        {
+            transform.parent.SendMessage("TurnRound");
+            print("xxx");
+        }
+    }
+}
