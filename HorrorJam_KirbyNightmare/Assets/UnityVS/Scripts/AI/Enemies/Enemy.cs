af436431dd971ab1be2ec067f190ceaaac13f191
HackerEva <565430601@qq.com>
2015-10-24 00:30:46 -0400

    Enemy 1

---
+++ HorrorJam_KirbyNightmare/Assets/UnityVS/Scripts/AI/Enemies/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    protected Rigidbody2D myRigibody;
+    public float speed = 4;
+    
+    // Use this for initialization
+
+    void Start () {
+        
+
+
+
+    }
+
+    protected void iniThis() {
+        myRigibody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    protected void Move() {
+        myRigibody.AddForce(new Vector2(speed, 0));
+    }
+
+    protected void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.tag == "Player")
+        {
+            SendMessage("Attack");
+        }
+
+        if (coll.collider.tag == "Block")
+        {
+            speed *= -1;
+        }
+    }
+
+}
---
+++ HorrorJam_KirbyNightmare/Assets/UnityVS/Scripts/AI/Enemies/WaddleDoo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaddleDoo : Enemy {
+
+    // Use this for initialization
+    void Start () {
+        iniThis();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    
+}
