7216fc639b69266483ec8eb6ee81f4416b61603c
tlatsa <tlatsa@knights.ucf.edu>
2015-10-25 12:33:22 -0400

    forgot some stuff

--- HorrorJam_KirbyNightmare/Assets/KirbyPowers.cs
+++ HorrorJam_KirbyNightmare/Assets/KirbyPowers.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class KirbyPowers : MonoBehaviour {
 
+	public KirbyCode Player;
+	public KirbyStates ability;
 
 	// Use this for initialization
 	void Start () {
-	
+		ability = Player.currState;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void AbilitySwitch() {
 	}
 }
