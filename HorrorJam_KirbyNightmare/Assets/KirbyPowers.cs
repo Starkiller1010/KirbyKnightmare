d178bd3a3fc2a0a2628f9b655042f79ad8e6d1a4
tlatsa <tlatsa@knights.ucf.edu>
2015-10-25 13:31:33 -0400

    spark

--- HorrorJam_KirbyNightmare/Assets/KirbyPowers.cs
+++ HorrorJam_KirbyNightmare/Assets/KirbyPowers.cs
@@ -13,9 +13,19 @@ public class KirbyPowers : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (ability != Player.currState) {
+			ability = Player.currState;
+			AbilitySwitch();
+		}
 	}
 
 	void AbilitySwitch() {
+		switch (ability) {
+			case KirbyStates.SPARK:
+			CircleCollider2D sparks = Player.gameObject.AddComponent<CircleCollider2D>();
+			sparks.radius = 5;
+			break;
+
+		}
 	}
 }
