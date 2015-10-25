4d87ad3865277a90e99bcb0ab55c2d6dc15e1b64
tlatsa <tlatsa@knights.ucf.edu>
2015-10-25 12:50:04 -0400

    all freshened

--- HorrorJam_KirbyNightmare/Assets/KirbyLives.cs
+++ HorrorJam_KirbyNightmare/Assets/KirbyLives.cs
@@ -5,16 +5,22 @@ using System.Collections;
 public class KirbyLives : MonoBehaviour {
 
 	public Text lifeLine;
+	public KirbyCode Player;
+	private int lives;
 	// Use this for initialization
 	void Start () {
+		lives = Player.lives;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lives != Player.lives) {
+			lives = Player.lives;
+			UpdateLives ();
+		}
 	}
 
-	void UpdateLives(int lives) {
+	void UpdateLives() {
 		lifeLine.text = "x" + lives.ToString ().PadLeft (2, '0');
 	}
 }
