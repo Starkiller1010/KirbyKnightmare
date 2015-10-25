d374db4a6c67d5f6b54a38346e2d142be48e7130
tlatsa <tlatsa@knights.ucf.edu>
2015-10-25 17:10:49 -0400

    yahoo

--- HorrorJam_KirbyNightmare/Assets/BossHealthBar.cs
+++ HorrorJam_KirbyNightmare/Assets/BossHealthBar.cs
@@ -5,21 +5,32 @@ using System.Collections;
 public class BossHealthBar : MonoBehaviour {
 
 	public Slider bossHealth;
+	public Boss baws;
+	private int health;
 
 	// Use this for initialization
 	void Start () {
+		bossHealth = baws.healthcount;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (health != baws.healthcount) {
+			health = baws.healthcount;
+			UpdateBossHealth ();
+		}
 	
 	}
 
-	void UpdateBossHealth(int healthLeft) {
-		if (healthLeft > 100)
-			healthLeft = 100;
-		if (healthLeft < 0)
-			healthLeft = 0;
-		bossHealth.value = (float)healthLeft/100;
+	void UpdateBossHealth() {
+		if (health > 5)
+			health = 5;
+		if (health < 0)
+			health = 0;
+		bossHealth.value = (float)health/5;
+		if(health == 0)
+			bossHealth.fillRect.gameObject.SetActive (false);
+		else
+			bossHealth.fillRect.gameObject.SetActive (true);
 	}
 }
