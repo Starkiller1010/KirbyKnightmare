19d15372e48cbd7f53c40a0d07aae31a5adc95a0
Winston Todd <wmt814@gmail.com>
2015-10-24 22:32:31 -0400

    Extracted the hopping and player detection behaviors into their own scripts, so they can be used for other enemies

--- HorrorJam_KirbyNightmare/Assets/Enemies/Scarfy.cs
+++ HorrorJam_KirbyNightmare/Assets/Enemies/Scarfy.cs
@@ -4,83 +4,25 @@ using Assets.Util;
 using System.Collections.Generic;
 
 public class Scarfy : MonoBehaviour {
-    const float JUMP_INTERVAL = 2; // seconds
-    const float TURN_INTERVAL = 8; // seconds
     Animator animator;
-    Rigidbody2D body;
-    Direction direction = Direction.RIGHT;
-    RepeatedAction jumpAction;
-    RepeatedAction turnAction;
-    
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
-        body = GetComponent<Rigidbody2D>();
-
-
-        jumpAction = new RepeatedAction(Jump, JUMP_INTERVAL, this);
-        turnAction = new RepeatedAction(TurnAround, TURN_INTERVAL, this);
-
-        jumpAction.Start();
-        turnAction.Start();
-	}
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 
-	}
-
-    void OnPlayerEnterAttentionRange()
-    {
-        print("Player entered attention range");
     }
 
-    void Attack()
+    void OnPlayerExitRange()
+    {
+        animator.SetBool("isAttacking", false);
+    }
+
+    void OnPlayerEnterRange()
     {
         animator.SetBool("isAttacking", true);
-    }
-
-    void Jump()
-    {
-        float x = direction == Direction.RIGHT ? 50.0f : -50.0f;
-        body.AddForce(new Vector2(x, 100.0f));
-    }
-
-    void TurnAround()
-    {
-        if (direction == Direction.LEFT)
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-            direction = Direction.RIGHT;
-        }
-        else
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-            direction = Direction.LEFT;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            print("Player entered attention range");
-            animator.SetBool("isAttacking", true);
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            animator.SetBool("isAttacking", false);
-        }
-    }
-
-    private enum Direction
-    {
-        LEFT,
-        RIGHT
     }
 }
